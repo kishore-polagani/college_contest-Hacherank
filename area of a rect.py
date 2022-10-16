816de3a2a9a823941ecad592751ea1d28d73e307
kishore-polagani <94750532+kishore-polagani@users.noreply.github.com>
2022-10-16 10:06:50 +0530

    Create area of a rect.py

---
+++ area of a rect.py
@@ -0,0 +1,25 @@
+Given the length and breadth of a rectangle, print area of the rectangle.
+
+Input Format
+
+First and only line of input contains two positive integers L - length of the rectangle and B - breadth of the rectangle.
+
+Constraints
+
+1 <= L, B <=109
+
+Output Format
+
+Print area of the given rectangle.
+
+Sample Input 0
+
+5 8
+Sample Output 0
+
+40
+...
+x=list(map(int,input().split()))
+y=x[0]
+z=x[1]
+print(y*z)
