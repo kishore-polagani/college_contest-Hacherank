1171b26772cda6a33ee831da5f0e1a17893f4a37
kishore-polagani <94750532+kishore-polagani@users.noreply.github.com>
2022-10-16 10:49:01 +0530

    Create Nth fibonacci number.py

---
+++ Nth fibonacci number.py
@@ -0,0 +1,30 @@
+For a given positive integer - N, compute Nth fibonacci number.
+
+Input Format
+
+First and only line of input contains a positive number - N.
+
+Constraints
+
+1 <= N <= 20
+
+Output Format
+
+Print the Nth fibonacci number.
+
+Sample Input 0
+
+4
+Sample Output 0
+
+3
+...
+def Fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return (Fibonacci(n-1)+Fibonacci(n-2))
+n=int(input())
+print(Fibonacci(n))
