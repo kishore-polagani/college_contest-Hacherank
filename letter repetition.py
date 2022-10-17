e319fa3fd456b14a42083d4aa308f941db353ba8
kishore-polagani <94750532+kishore-polagani@users.noreply.github.com>
2022-10-17 21:22:10 +0530

    Create letter repetition.py

---
+++ letter repetition.py
@@ -0,0 +1,16 @@
+Given an integer - N, print a string with the letter 'o' repeated for N times in the word Go...od.
+
+Input Format
+
+First and only line of input contains a single integer N.
+
+Constraints
+
+0 <= N<= 100
+
+Output Format
+
+Print the word Go..od with letter 'o' repeated N times.
+...
+n=int(input())
+print('G','o'*n,'d',sep='')
